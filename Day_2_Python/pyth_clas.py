abfe8b8126d36b7391ec2616fcdbcb8ca91def88
Sandhya Rai <sandhyarai@Sandhyas-MacBook-Air.local>
2026-01-18 09:05:14 -0800

    Day-2 work

---
+++ Day_2_Python/pyth_clas.py
@@ -0,0 +1,16 @@
+class Student:
+    def greetings(self):
+        print("gteeting from python")
+
+    def anotherGreet(self, msg):
+        print(msg)
+         
+
+
+
+
+stu = Student()     
+print(stu.greetings()) 
+
+#gree = "HI THIS GREET IS FROM ANOTHER GREET "
+stu.anotherGreet("Hi THIS GREETING IS FRON ANOTHER GREETING")
